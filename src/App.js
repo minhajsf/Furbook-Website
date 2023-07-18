1a5d3b4f49ca1194891881b838163bcb8498bd62
Minhaj Fahad <111889497+minhajsf@users.noreply.github.com>
2023-07-18 15:15:42 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import PetNav from "./components/pet-nav";
+import PictureBox from "./components/picture-box";
+import AboutBox from "./components/about-box";
+import Posts from "./components/posts";
+import "./App.css";
+
+const App = () => {
+  const component = new React.Component();
+
+  component.render = () => {
+    return (
+      <div className="App">
+        <PetNav />
+        <div class="container">
+          <div class="row">
+            <PictureBox />
+            <AboutBox />
+          </div>
+          <div class="row">
+            <Posts />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return component;
+};
+
+export default App;
